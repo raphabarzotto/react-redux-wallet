04e64e6a20ad871db12c37a88ad79eff36b172bb
raphabarzotto <raphabarzotto@gmail.com>
2023-01-09 19:40:43 -0300

    feat: login page css 2 - now in right files

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
+import './login.css'
 
 import { loginButtonPress } from '../actions';
 
