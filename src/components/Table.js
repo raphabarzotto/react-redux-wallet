26ff1cddb927d69d2bc4202f5691801bd0298f64
raphabarzotto <raphabarzotto@gmail.com>
2023-01-10 17:02:40 -0300

    feat: change language do english 2

--- src/components/Table.js
+++ src/components/Table.js
@@ -6,49 +6,52 @@ class Table extends React.Component {
   render() {
     const { expenses } = this.props;
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>Description</th>
-            <th>Tag</th>
-            <th>Payment Method</th>
-            <th>Value</th>
-            <th>Currency</th>
-            <th>Exchange</th>
-            <th>Converted Value</th>
-          </tr>
-        </thead>
-        <tbody>
-          { expenses.map((expense) => {
-            const {
-              value,
-              currency,
-              method,
-              tag,
-              description,
-              id,
-              exchangeRates,
-            } = expense;
-            const expenseCurrencyValue = exchangeRates[currency];
-            const totalValue = value * expenseCurrencyValue.ask;
-            const currencyValue = Number(expenseCurrencyValue.ask).toFixed(2);
-            const valueToFixed = Number(value).toFixed(2);
-            const totalValueToFixed = Number(totalValue).toFixed(2);
-
-            return (
-              <tr key={ id }>
-                <td>{ description }</td>
-                <td>{ tag }</td>
-                <td>{ method }</td>
-                <td>{ valueToFixed }</td>
-                <td>{ expenseCurrencyValue.name }</td>
-                <td>{ currencyValue }</td>
-                <td>{ totalValueToFixed }</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <div className="table-div">
+        <table>
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Tag</th>
+              <th>Payment Method</th>
+              <th>Value</th>
+              <th>Currency</th>
+              <th>Exchange</th>
+              <th>Converted Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            { expenses.map((expense, index) => {
+              const {
+                value,
+                currency,
+                method,
+                tag,
+                description,
+                id,
+                exchangeRates,
+              } = expense;
+              console.log((index + 1) % 2 === 1);
+              const expenseCurrencyValue = exchangeRates[currency];
+              const totalValue = value * expenseCurrencyValue.ask;
+              const currencyValue = Number(expenseCurrencyValue.ask).toFixed(2);
+              const valueToFixed = Number(value).toFixed(2);
+              const totalValueToFixed = Number(totalValue).toFixed(2);
+              
+              return (
+                <tr key={ id }>
+                  <td>{ description }</td>
+                  <td>{ tag }</td>
+                  <td>{ method }</td>
+                  <td>{ valueToFixed }</td>
+                  <td>{ expenseCurrencyValue.name }</td>
+                  <td>{ currencyValue }</td>
+                  <td>{ totalValueToFixed }</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
